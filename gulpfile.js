c75cd333d3d3f04c34c7b81b99febf9bfed659bc
Alvin Raminger <alvin.raminger@snk.de>
2020-06-05 08:57:01 +0200

    Updated Styling, added gulp-file with tasks to copy documentation-contents from remote and generate pages referencing md-files

--- gulpfile.js
+++ gulpfile.js
@@ -14,18 +14,20 @@ const webpack2 = require('webpack');
 const named = require('vinyl-named');
 const uncss = require('uncss');
 const autoprefixer = require('autoprefixer');
-
+const dirTree = require("directory-tree");
 // Load all Gulp plugins into one variable
 const $ = plugins();
 
 // Check for --develop or --dev flag
-var PRODUCTION = !(yargs.argv.develop || yargs.argv.dev);
+let PRODUCTION = !(yargs.argv.develop || yargs.argv.dev);
 
 // Load settings from settings.yml
 const { COMPATIBILITY, PORT, UNCSS_OPTIONS, PATHS } = loadConfig();
+let filenotes;
+// const DOCS_INIT = (yargs.argv.initialize || yargs.argv.init);
 
-function loadConfig() {
-  let ymlFile = fs.readFileSync('config.yml', 'utf8');
+function loadConfig(config) {
+  let ymlFile = fs.readFileSync(config ? config : 'config.yml', 'utf8');
   return yaml.load(ymlFile);
 }
 
@@ -155,10 +157,10 @@ function server(done) {
   browser.init(
     {
       server: {
-          baseDir: PATHS.dist,
-          serveStaticOptions: {
-              extensions: ['html']
-          }
+        baseDir: PATHS.dist,
+        serveStaticOptions: {
+          extensions: ['html']
+        }
       },
       port: PORT
     },
@@ -194,4 +196,389 @@ function watch() {
   gulp
     .watch('src/assets/img/**/*')
     .on('all', gulp.series(images, browser.reload));
+}
+
+// Pull Documentation Pages (Markdown) and included Images from Remotes defined in docs.json.
+gulp.task("docs-remotes",
+  gulp.series(getSourceDocs, setSourceDocsImageReferences, getSourceDocsImages)
+);
+
+// Generate Dokumentation Pages in pages-folder as html, referencing the md-file. Settings in docs.json.
+gulp.task("docs-pages",
+  gulp.series(cleanDocsPages, createDocsPages)
+);
+
+function getSourceDocsImages(){
+  return getSourceDocs();
+}
+
+function setSourceDocsImageReferences() {
+  const updateBase = function (link) {
+    if (!base) {
+      base = link.substring(0, link.lastIndexOf("/") + 1);
+    } else {
+      if (link.indexOf(base) < 0 && base.indexOf("/") > 0) {
+        base = base.substring(0, base.length - 1);
+        base = base.substring(0, base.lastIndexOf("/") + 1);
+        updateBase(link);
+      }
+      if (base.indexOf("/") < 0) {
+        console.log("No common base found: invalid.");
+        base = false;
+      }
+    }
+  },
+  getImagePath = function (resourceUrl, imageUrl, fwd = false) {
+    if (imageUrl.indexOf("http:") == 0 || imageUrl.indexOf("https:") == 0) {
+      console.log("external file, returned unchanged.");
+    } else {
+      resourceUrl = resourceUrl.substring(0, resourceUrl.lastIndexOf("/"));
+      imageUrl = imageUrl.replace(/^(\.\/)/, "");
+      if (imageUrl.indexOf("../") == 0 && imageUrl.length > 5) {
+        return getImagePath(resourceUrl, imageUrl.substring(3, imageUrl.length));
+      }
+      if (imageUrl.indexOf("/") == 0) {
+        console.log("image from server-root detected.");
+        resourceUrl = resourceUrl.replace(/^((http:\/\/|https:\/\/)?[^\/]+).*/, "$1");
+      }
+      imageUrl = resourceUrl + "/" + imageUrl;
+    }
+    return imageUrl;
+  };
+  let current, imagePaths = { remotes: [], use: {}, names: {} , dest: PATHS.srcDocs.assets, files: [] }, base = false, currentUse;
+  return gulp.src(filenotes.files, { base: "." })
+    .pipe($.rename(function (path) {
+      current = filenotes.base + filenotes.remotes[filenotes.files.indexOf((path.dirname !== "." ? (path.dirname + "/") : "") + path.basename + path.extname)];
+      currentUse = (filenotes.use[current] ? filenotes.use[current] : false);
+    }))
+    .pipe($.edit(function (src, cb) {
+      const _err = null, _links = src.match(/\[.*\]\([^\)]+\)/g);
+      if (_links) {
+        for (var m in _links) {
+          const _imageMDdefinitions = _links[m].match(/\[.+\]\(([^\s]+\.(png|svg|jpg|gif)).*\)/);
+          if (_imageMDdefinitions && _imageMDdefinitions.length > 1) {
+            const _imgPath = getImagePath(current, _imageMDdefinitions[1]),
+              _imgName = _imgPath.substring(_imgPath.lastIndexOf("/") + 1, _imgPath.length);
+            imagePaths.remotes.push(_imgPath);
+            updateBase(_imgPath);
+            if (!imagePaths.use[_imgPath]) {
+              imagePaths.use[_imgPath] = [];
+            }
+            if (imagePaths.names[_imgName]) {
+              _imgName = (currentUse ? currentUse : "") + "_" + _imgName;
+            }
+            imagePaths.names[_imgPath] = {original: _imageMDdefinitions[1], name: _imgName};
+            imagePaths.files.push(_imgName);
+            imagePaths.use[_imgPath].push({ remote: current, data: currentUse, name: _imgName, original: _imageMDdefinitions[1] });
+          }
+        }
+        for (var n in imagePaths.names) {
+          const _rep = "(/" + imagePaths.dest.replace(/^src\/?/,'') + imagePaths.names[n].name,
+            _orig = "(" + imagePaths.names[n].original;
+          src = src.replace(_orig, _rep); //initial "/" for absolute path
+          if(!PRODUCTION){
+            console.log(src);
+          }
+        }
+      }
+      cb(_err, src);
+    })).pipe(gulp.dest("./"))
+    .on("end", function () { 
+      filenotes.dest = imagePaths.dest;
+      filenotes.remotes = imagePaths.remotes;
+      filenotes.base = base;
+      filenotes.files = imagePaths.files;
+      filenotes.names = imagePaths.names;
+      filenotes.use = imagePaths.use;
+      filenotes.structure = false;
+      filenotes.step = function (rurl, lurl, url) {
+        const _retobj = { dirname: ".", basename: imagePaths.names[rurl].name};
+        _retobj.basename = _retobj.basename.substring(0, _retobj.basename.lastIndexOf("."));
+        return _retobj;
+      }
+    });
+}
+
+function getSourceDocs() {
+  let remotes = [], base, use = {}, filenames = {}, dest, data, writeDataJSOM, postNamingStep;
+  if(!filenotes){
+    data = getDocsStructure();
+    writeDataJSOM = true;
+    dest = PATHS.srcDocs.sources;
+    const updateBase = function (link) {
+      if (!base) {
+        base = link.substring(0, link.lastIndexOf("/") + 1);
+      } else {
+        if (link.indexOf(base) < 0 && base.indexOf("/") > 0) {
+          base = base.substring(0, base.length - 1).substring(0, base.lastIndexOf("/") + 1);
+          updateBase(link);
+        }
+        if (base.indexOf("/") < 0) {
+          console.log("No common base found: invalid.");
+          base = false;
+        }
+      }
+    },
+    getRemoteLinks = function (structure, langs) {
+      loopStructureDoc("source", function (val, key, lang, info){
+        if (remotes.includes(val)) {
+          use[val].push(info.container);
+        } else {
+          remotes.push(val);
+          updateBase(val);
+          let _filename = val.substring(val.lastIndexOf("/") + 1, val.length).toLowerCase();
+          if (_filename.length > 0) {
+            if (filenames[_filename]) {
+              if (!filenames[lang + "/" + _filename]) {
+                _filename = lang + "/" + _filename;
+              } else {
+                if (!key) console.log("Error capturing File: " + val);
+                _filename = lang + "/" + key + "/" + _filename;
+              }
+            }
+            info.container.tmpFilename = _filename;
+          }
+          use[val] = [info.container];
+        }
+      }, false, structure, langs);
+    }
+    getRemoteLinks(data.structure, data.languages.types);
+    filenames = [];
+    postNamingStep = function (r_url, l_url, url) {
+      use[r_url].forEach(element => {
+        element.path = l_url;
+        element.tmpFilename = undefined;
+      });
+    }
+  }else{
+    dest = filenotes.dest;
+    filenames = [];//filenotes.files;
+    remotes = filenotes.remotes;
+    base = filenotes.base;
+    writeDataJSOM = filenotes.write;
+    data = filenotes.structure;
+    postNamingStep = filenotes.step;
+    use = filenotes.use;
+  }
+  if (base) {
+    console.log("creating on base: " + base);
+    remotes = remotes.join(", ").replace(new RegExp(base, 'g'), '').split(", ");
+    let remoteSrc = function (sources, base, step, end, syncArray, syncObj) {
+      if (!end) {
+        end = function () { console.log("done with addressing remotes.") }
+      }
+      if (!syncArray) {
+        syncArray = filenames;
+      }
+      if (!syncObj) {
+        syncObj = use;
+      }
+      return $.remoteSrc(sources, { base: base })
+        .pipe($.rename(function (path) {
+          const url = (path.dirname != "." ? path.dirname + "/" : "") + path.basename + path.extname,
+            remoteurl = base + url;
+          let local_url = dest + url;
+          if (syncObj && syncObj[remoteurl] && syncObj[remoteurl].length > 0) {
+            const _localBasename = syncObj[remoteurl][0].tmpFilename;
+            if (_localBasename) {
+              local_url = dest;
+              if (_localBasename.indexOf("/") > 0) {
+                path.dirname = _localBasename.substring(0, _localBasename.lastIndexOf("/"))
+                path.basename = _localBasename.substring(_localBasename.lastIndexOf("/") + 1, _localBasename.lastIndexOf("."));
+                local_url += path.dirname + "/";
+              } else {
+                path.dirname = ".";
+                path.basename = _localBasename.substring(0, _localBasename.lastIndexOf("."));
+              }
+              local_url += path.basename + path.extname;
+            }
+            if (typeof (step) == "function") {
+              let _result = step(remoteurl, local_url, url, path);
+              if (_result) {
+                if (_result.basename) path.basename = _result.basename;
+                if (_result.dirname) path.dirname = _result.dirname;
+                local_url = dest + (path.dirname != "." ? path.dirname + "/" : "") + path.basename + path.extname;
+              }
+            }
+          }
+          syncArray.push(local_url);
+        }))
+        .pipe(gulp.dest(dest))
+        .on("end", end);
+    }
+    return remoteSrc(remotes, base, postNamingStep, function () {
+      if (!PRODUCTION){ 
+        console.log("writing files");
+        console.log(filenames);
+      }
+      if (writeDataJSOM){
+        writeDocsStructure(data);
+      }
+      filenotes = { remotes: remotes, base: base, files: filenames, structure: data, use: use };
+    }, filenames, use);
+  }
+}
+
+function writeDocsStructure(data) {
+  if (!data && typeof (data) !== 'object') { console.log("invalid data, " + PATHS.srcDocs.structure.file + " not written."); return false; }
+  console.log("writing " + PATHS.srcDocs.structure.folder + PATHS.srcDocs.structure.file);
+  fs.writeFileSync(
+    PATHS.srcDocs.structure.folder + PATHS.srcDocs.structure.file,
+    JSON.stringify(data, null, "\t")
+  );
+  return true;
+}
+
+function getDocsStructure() {
+  return JSON.parse(
+    fs.readFileSync(
+      PATHS.srcDocs.structure.folder + PATHS.srcDocs.structure.file,
+      "utf8"
+    )
+  );
+}
+
+function getPagePropertiesHeader(pageInfo, propertiesDef) {
+  let props = "---\nlayout: docs\n";
+
+  props += '---\n';
+  return props;
+}
+
+function loopStructureDoc(attribute, reply, lang, structure, languages, nesting, namepath=""){
+  if (!structure && filenotes && filenotes.data && filenotes.data.structure){
+    structure = filenotes.data;
+  }
+  const _languages = languages ? languages : structure.languages.types,
+    _structure = structure.structure ? structure.structure : structure;
+  for (var k in _structure) {
+    if (typeof (_structure[k]) === "string" && k === attribute) {
+      reply(_structure[k], (nesting ? nesting.substring(nesting.lastIndexOf(".")+1, nesting.length) : false), lang, {languages: languages, path: nesting, container: _structure, namepath: namepath});
+    } else {
+      if (typeof (_structure[k]) === "object") {
+        loopStructureDoc(attribute, reply, (lang ? lang : (_languages.includes(k) ? k : false)), _structure[k], _languages, (nesting ? (nesting + "." + k) : k), _structure[k].name ? namepath + (_structure[k].name + "/") : namepath);
+      }
+    }
+  }
+}
+
+function getNestedObject (obj, path) {
+  var current = obj;
+  try {
+    path.split('.').forEach(function (p) { current = current[p]; });
+  } catch (e) { return false; }
+  return current;
+};
+
+function cleanDocsPages(){
+  const languages = getDocsStructure().languages.types;
+  let folders = [];
+  languages.forEach(language => {
+    folders.push(PATHS.srcDocs.pages + "/" + language + "/docs")
+  });
+  return gulp.src(folders, { read: false, allowEmpty: true })
+    .pipe($.clean());
+}
+
+async function createDocsPages() {
+  const config = getDocsStructure(), structure = config.structure;
+  if (typeof (structure) === "object") {
+    console.log('building page structure for docs');
+    let destinations = {}, contents = {};
+    const dest = PATHS.srcDocs.pages,
+      keyFolder = "docs",
+      langs = config.languages,
+      pageDefaults = config.pages;
+      
+    loopStructureDoc('path', function(val, key, lang, info){
+      let _notes = {title: info.container.title, name: info.container.name, path: val, description: info.container.description};
+      _notes.key = info.path.indexOf(lang) == 0 ? info.path.replace(lang + ".", "") : info.path;
+      _notes.url = lang + "/" + keyFolder + "/" + (key !== config.index ? info.namepath : "");
+      _notes.language = lang;
+      if(!_notes.description && config.description && config.description[lang]){
+        _notes.description = config.description[lang];
+      }
+      destinations[dest + "/" + _notes.url + "index.html"] = _notes;
+      contents[info.path] = _notes;
+    }, false, config);
+
+    for(let file in destinations){
+      const details = destinations[file];
+      let filecontent = "---\nlayout: docs\n",
+        getOtherLanguageValue = function (val, call) {
+          const obj = {};
+          langs.types.forEach((language) => {
+            if (language != details.language) {
+              if (val) {
+                obj[language] = contents[language + "." + details.key][val];
+              }
+              call(language, val ? obj[language] : false);
+            }
+          });
+          return obj;
+        },
+        getMatched = function (str) {
+          const match = str.match(/\[?#\{[^\}]+\}\]?/g);
+          if (match) {
+            let _strs = [],
+              _str = str;
+            match.forEach((item) => {
+              if (item.indexOf("#{language.types") >= 0) {
+                if (item.indexOf(":") > item.indexOf("#{language.types")) {
+                  getOtherLanguageValue(
+                    item.replace(/#\{language\.types:#([^\}]+)\}/, "$1"),
+                    function (lang, langContent) {
+                      _strs.push(str.replace(item, langContent));
+                    }
+                  );
+                } else {
+                  getOtherLanguageValue(false, function (a) {
+                    _strs.push(_str.replace(item, a));
+                  });
+                }
+              } else {
+                let strValue = details[item.replace(/#\{([^\}]+)\}/, "$1")];
+                if (_strs.length > 0) {
+                  for (let i = 0; i < _strs.length; i++) {
+                    _strs[i] = _strs[i].replace(item, strValue);
+                  }
+                } else {
+                  _str = _str.replace(item, strValue);
+                }
+              }
+            });
+            if (_strs.length > 0) {
+              return _strs;
+            } else {
+              return _str;
+            }
+          }
+          return str;
+        };
+      for (let p in pageDefaults.properties) {
+        let prop = getMatched(p), 
+          val = getMatched(pageDefaults.properties[p]);
+        if(prop instanceof Array && prop.length > 1 && val instanceof Array && val.length == prop.length){
+          for(let m=0; m<val.length; m++){
+            filecontent += prop[m] + ": " + val[m] + "\n";
+          }
+        }else{
+          filecontent += prop + ": " + val + "\n";
+        }
+      }
+      filecontent +="---\n{{{md '" + details.path + "'}}}\n\n";
+      console.log("creating file: " + file);
+      let targetDir = file.substr(0, file.lastIndexOf("/"));
+      if (!fs.existsSync(targetDir)){
+        fs.mkdirSync(targetDir, { recursive: true });
+      }
+      fs.writeFileSync(
+        file,
+        filecontent,
+        (err) => {
+          if (err) throw err;
+        }
+      );
+    }
+  }
 }
--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,5 +1,6 @@
 import $ from 'jquery';
 import './lib/slick.min.js';
+import "./lib/jquery.inview.js";
 //import 'what-input';
 
 window.jQuery = $;
@@ -11,8 +12,24 @@ $(".js-docs .code").append('<button class="btn btn_clipboard-copy js-clipboard">
 $('.js-docs > h1:first-child + ol, .js-docs > h2:first-child + ol, .js-docs > ol:first-child').each(function(){
     if($(this).find("li>a").length == $(this).find("a").length){
         const menu = $(this).parents('.js-docs').prev('.js-menu');
+        $(this).find("li>a").each(function(){
+            const link = $(this).attr("href");
+            if(link.indexOf("#") == 0){
+                if($(".js-docs " + link).index() < 0){
+                    const text = $(this).text();
+                    $('.js-docs').find('h1,h2,h3,h4,h5,h6').each(function(){
+                        if($(this).text() == text){
+                            $(this).attr('id', link.substring(1, link.length));
+                            return false;
+                        }
+                    });
+                }
+                $('.js-docs ' + link).prepend('<span class="js-page-anchor"></span>');
+            }
+        });
         $(this).parents('.js-docs').after('<div class="sidebar-toc"></div>');
         $(this).addClass('toc-list js-toc-list js-menu');
+        $(this).find("a").first().addClass('active');
         $(this).find("li").addClass('js-scroll-navigate');
         $(this).find('ol').addClass('js-menu');
         if($(this).prev()[0].nodeName.toLowerCase() == "h1" || $(this).prev()[0].nodeName.toLowerCase() == "h2"){
@@ -24,15 +41,26 @@ $('.js-docs > h1:first-child + ol, .js-docs > h2:first-child + ol, .js-docs > ol
         }
     }
 });
+$(".js-docs table").each(function(){
+    if ($(this).find("th").first().text().indexOf("#") == 0 && $(this).find("thead th").length == 3) {
+        $(this).addClass('data-table');
+        if($(this).find("th").first().text().length > 4){
+            $(this).addClass("data-table_detailed");
+        }
+      //console.log("presumably a data table");
+    }
+});
 $(document).ready(function(){
     $('.js-accordion dt, .js-toggle').on('click tap', function(){
         $($(this).data('target') ? $(this).data('target') : $(this)).toggleClass('active');
     });
-    $('.js-menu .js-scroll-navigate a').on('click tap', function(){
-        $(this).parents('.js-menu').first().removeClass('active');
-        $(this).parents('.js-menu').first().find('a').removeClass('active');
-        $(this).addClass('active');
-    });
+    const setActiveMenuItem = function(){
+        $(this).parents(".js-menu").first().removeClass("active");
+        $(this).parents(".js-menu").first().find("a").removeClass("active");
+        $(this).addClass("active");
+        console.log($(this).text() + " activated");
+    }
+    $(".js-menu .js-scroll-navigate a").on("click tap", setActiveMenuItem);
     $('.js-slider').slick({
         dots: true,
         infinite: true,
@@ -46,6 +74,32 @@ $(document).ready(function(){
                 slidesToScroll: 1
             }
         }]
-    });    
+    });
+    if($('.js-page-anchor').length > 1){
+        let scrollupdate = true;
+        $(".js-toc-list a").on('click tap', function(){
+            scrollupdate = false;
+            $(setTimeout(() => {
+                scrollupdate = true;
+            }, 300));
+        });
+        $('.js-page-anchor').on('inview', function(event, isInView){
+            if(isInView && scrollupdate){
+                $('.js-toc-list').each(function(){
+                    if(event.target.id){
+                        setActiveMenuItem.call(
+                          $(this).find(
+                            'a[href="#' + event.target.id + '"]'
+                          )
+                        );
+                    }else{
+                        setActiveMenuItem.call($(this).find(
+                        'a[href="#' + event.target.parentNode.id + '"]'
+                        ));
+                    }
+                });
+            }
+        });
+    }
 });
 
